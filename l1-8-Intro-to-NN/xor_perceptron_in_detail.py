067e4bd070e3b2a7675331a450ab79294c91e111
perrass <pixelss@163.com>
2017-06-06 09:51:11 +0800

    6.6 add forward and backpropagation in detail

--- l1-8-Intro-to-NN/xor_perceptron_in_detail.py
+++ l1-8-Intro-to-NN/xor_perceptron_in_detail.py
@@ -1,20 +1,33 @@
-from numpy import exp, dot, random, array
+import numpy as np
 import pandas as pd
 
 
-class XorPerceptron():
+class XorPerceptron(object):
 
     def __init__(self):
+        """
+        初始化神经元
+        weights: 第一层有四个神经元位，两个是有权重的神经元，两个是Pass，
+                 第二层两个神经元，第三层一个
+        bias: weights对应的偏置
+        activate_function: 激活函数，单位跃阶函数 (Heavisible step function)
+        linear_combination: 神经元的计算方式
+        """
         self.weights = ((0.0, -1.0), (0.0, -1.0),
                         (1.0, 1.0), (1.0, 1.0), (1.0, 1.0))
         self.bias = (0.0, 0.0, -2.0, -2.0, -1.0)
         self.activate_function = lambda x: int(x >= 0)
+        # x: weights, y: input, z: bias
         self.linear_combination = lambda x, y1, y2, z: \
             x[0] * y1 + x[1] * y2 + z
-        self.lc_value = 0.0
-        self.output = 0.0
 
-    def _output(self, test_input):
+    def output(self, test_input):
+        """
+        通过神经网络得到输出
+        :param test_input: 输入
+        :return: the value of linear_combination at the last layer, output
+        """
+        assert len(test_input) == 2
         lc_1_1 = self.linear_combination(
             self.weights[0], test_input[0], test_input[1], self.bias[0])
         lc_1_2 = self.linear_combination(
@@ -29,26 +42,34 @@ class XorPerceptron():
             self.activate_function(lc_2_2)
         lc_3 = self.linear_combination(
             self.weights[4], output_2_1, output_2_2, self.bias[4])
-        self.lc_value = lc_3
-        self.output = self.activate_function(lc_3)
 
-if __name__ == "__main__":
+        lc_value = lc_3
+        output = self.activate_function(lc_3)
+
+        return lc_value, output
+
+
+def run():
     test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
     correct_outputs = [False, True, True, False]
     outputs = []
 
     for test_input, correct_output in zip(test_inputs, correct_outputs):
         xor_perceptron = XorPerceptron()
-        xor_perceptron._output(test_input)
-        is_correct_string = 'Yes' if xor_perceptron.output == correct_output else 'No'
+        lc_value, xor_output = xor_perceptron.output(test_input)
+        is_correct_string = 'Yes' if xor_output == correct_output else 'No'
         outputs.append([test_input[0], test_input[1],
-                        xor_perceptron.lc_value, xor_perceptron.output, is_correct_string])
+                        lc_value, xor_output, is_correct_string])
 
     num_wrong = len([output[4] for output in outputs if output[4] == 'No'])
     output_frame = pd.DataFrame(outputs, columns=['Input1', 'Input2',
                                                   'Linear Combination', 'Output', 'Is Correct'])
     if not num_wrong:
-        print ('Nice! You got it all correct.\n')
+        print('Nice! You got it all correct.\n')
     else:
-        print ('You got {} wrong. Keep trying!\n'.format(num_wrong))
+        print('You got {} wrong. Keep trying!\n'.format(num_wrong))
     print(output_frame.to_string(index=False))
+
+
+if __name__ == "__main__":
+    run()
